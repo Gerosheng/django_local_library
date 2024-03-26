e1640908c92b423856196058407e0e53f5a1703e
Jérôme <jerome.coeck@outlook.com>
2024-03-26 17:04:09 +0100

    user authentication

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('book/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('author/', views.AuthorListView.as_view(), name = 'authors'),
-    path('author/<int:pk>', views.AuthorDetailViews.as_views(), name = 'author-detail')
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name = 'author-detail'),
 ]
